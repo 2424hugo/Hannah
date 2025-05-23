b2e6a253818af7ca44503fdbe3f2414730e2f658
2424hugo <hugoleighwatts@hotmail.co.uk>
2025-05-23 23:51:15 +0100

    Add files via upload

--- Hannah.py
+++ Hannah.py
@@ -3,10 +3,15 @@ from ai import AI
 from todo import Todo, Item
 from weather import Weather
 from randfacts import randfacts
+from datetime import datetime
+from calendar_skill import Calendar_skill
+import dateparser
  
 
 hannah = AI()
 todo = Todo()
+calendar = Calendar_skill()
+calendar.load()
 
 def facts():
     fact = randfacts.get_fact()
@@ -51,15 +56,83 @@ def remove_todo()->bool:
         print("oops there was an error")
         return False
 
+def add_event()->bool:
+    hannah.say("What is the name of the event")
+    try:
+        event_name = hannah.listen()
+        hannah.say("When is the evemt")
+        event_begin = hannah.listen()
+        event_isodate = dateparser.parse(event_begin).strftime("%Y-%m-%d %H:%M:%S")
+        hannah.say("What is the event description?")
+        event_description = hannah.listen()
+        message = "Ok, adding event " + event_name
+        hannah.say(message)
+        calendar.add_event(begin=event_isodate, name=event_name, description=event_description)
+        calendar.save()
+        return True
+    except:
+        print("opps there was an error") 
+        return False
+
+def remove_event()->bool:
+    hannah.say("What is the name of the event you want to remove?")
+    try:
+        event_name = hannah.listen()
+        try:
+            message = "Okay removing " + event_name
+            print(message)
+            hannah.say(message)
+            calendar.remove_event(event_name=event_name)
+            calendar.save()
+            return True
+        except:
+            hannah.say("Sorry I could not find the event", event_name)
+            return False
+    except:
+        print("1 opps there was an error")
+        return False
+
+def list_events(period):
+    this_period = calendar.list_events(period=period)
+    print(this_period)
+    if this_period is not False:
+        message = "There "
+        if len(this_period)>1:
+            message = message + "are " + len(this_period) + " events in the diary"
+        else:
+            message = message + "is 1 event in the diary"
+        hannah.say(message)
+        for event in this_period:
+            event_date = event.begin.datetime
+            weekday = datetime.strftime(event_date, "%A")
+            day = str(event.begin.datetime.day)
+            month = datetime.strftime(event_date, "%B")
+            year = datetime.strftime(event_date, "%Y")
+            time = datetime.strftime(event_date, "%I:%M %p")
+            name = event.name
+            description = event.description
+            message = "On " + weekday + " the " + day + " of " + month + " " + year + " at " + time + ", there is an event called " + name + " with an event description of " + description
+            hannah.say(message)
+    else:
+        hannah.say("There are no events booked")
+
+
 def weather():
     todays_weather = Weather()
     forecast = todays_weather.forecast()
     print(forecast)
     hannah.say(forecast)
 
+def next_weather():
+    tomorrows_weather = Weather()
+    forecast = tomorrows_weather.tomorows_forcast()
+    print(forecast)
+    hannah.say(forecast)
+
+
 
 command = ""
-hannah.say("Hello")
+hannah.say("Hello, I'm hannah! How can I help you today?")
 while True and command != "goodbye":
     try:
         command = hannah.listen()
@@ -82,10 +155,43 @@ while True and command != "goodbye":
         remove_todo()
     if command in ['weather today', "what's the weather like", "what's the weather today", "what's the weather like today"]:
         weather()
+    if command in ['weather tomorrow', "what's the weather like tomorrow", "what's the weather tomorrow"]:
+        next_weather()
     if command == "tell me a fact":
         facts()
-    #if command in ['good morning', 'good evening', 'good night']:
-    #    return True
+    if command in ['good morning', 'good afternoon', 'good evening', 'good night']:
+        now = datetime.now()
+        hr = now.hour
+        if hr >= 0 and hr <= 12:
+            message = "Morning"
+        if hr > 12 and hr <= 17:
+            message = "Afternoon"
+        if hr > 17 and hr <= 21:
+            message = "Evening"
+        if hr >= 21: message = "Night"
+
+        message = "Good " + message + " Hugo"
+        if hr >= 0 and hr <= 12:
+            hannah.say(message)
+            weather()
+        if hr >= 21:
+            next_weather()
+            hannah.say(message)
+        else:
+            hannah.say(message)
+    
+    # Calendar
+    if command in ["add event", "add to calendar", "new event", "add a new event"]:
+        add_event()
+    if command in ["remove event", "cancel event", "delete event"]:
+        remove_event()
+    if command in ["list events", "what's on this month", "what's coming up this month"]:
+        list_events(period='this month')
+    if command in ["what's on this week", "what's coming up this week","what's happening this week"]:
+        list_events(period='this week')
+    if command in ["list all events"]:
+        list_events(period='all')
+
 
 
 hannah.say("Goodbye")
--- weather.py
+++ weather.py
@@ -1,8 +1,11 @@
 # Weather skill AI
 
 import pyowm
+import numpy
+import pytz
+from collections import Counter
 from geopy import Nominatim, location
-from datetime import datetime
+from datetime import datetime, timedelta
 
 class Weather():
 
@@ -20,6 +23,7 @@ class Weather():
         city = "Bournemouth"
         country = "GB"
         self.__location = city + "," + country
+        self.uk_tz = pytz.timezone('Europe/London')
         loc = locator.geocode(self.__location)
         self.lat = loc.latitude
         self.long = loc.longitude
@@ -50,9 +54,10 @@ class Weather():
         min_temp = forecast.temperature('celsius')['temp_min']
         max_temp = forecast.temperature('celsius')['temp_max']
         humidity = forecast.humidity
-        sunrise = datetime.strptime(forecast.sunrise_time('iso')[:19], "%Y-%m-%d %H:%M:%S").strftime("%I:%M %p")
-        sunset = datetime.strptime(forecast.sunset_time('iso')[:19], "%Y-%m-%d %H:%M:%S").strftime("%I:%M %p")
-
+        sunrise_utc = datetime.fromisoformat(forecast.sunrise_time('iso').replace('Z', '+00:00'))
+        sunset_utc = datetime.fromisoformat(forecast.sunset_time('iso').replace('Z', '+00:00'))
+        sunrise = sunrise_utc.astimezone(self.uk_tz).strftime('%I:%M %p')
+        sunset = sunset_utc.astimezone(self.uk_tz).strftime('%I:%M %p')
         #print(detailed_status)
         #print(wind_speed)
         #print(wind_direction)
@@ -70,11 +75,57 @@ class Weather():
             + ", sunset is at " + str(sunset)
         
         return message
+    
+    def tomorows_forcast(self):
+        "Returns the forecast for tomorrow"
+
+        current_weather = self.mgr.weather_at_place(self.__location).weather
+        sunrise_utc = datetime.fromisoformat(current_weather.sunrise_time('iso').replace('Z', '+00:00'))
+        sunset_utc = datetime.fromisoformat(current_weather.sunset_time('iso').replace('Z', '+00:00'))
+        sunrise = sunrise_utc.astimezone(self.uk_tz).strftime('%I:%M %p')
+        sunset = sunset_utc.astimezone(self.uk_tz).strftime('%I:%M %p')
+
+        forecast = self.mgr.forecast_at_place(self.__location, '3h')
+        tomorrow = datetime.utcnow().date() + timedelta(days=1)
+
+        weather_list = []
+
+        for weather in forecast.forecast:
+            weather_time = datetime.utcfromtimestamp(weather.reference_time())
+
+            if weather_time.date() == tomorrow and 9 <= weather_time.hour <= 21:
+                wind = weather.wind()
+                temp = weather.temperature('celsius')
+                entry = {
+                    'time': weather_time.strftime('%I:%M %p'),
+                    'temperature': temp.get('temp'),
+                    'temp_min': temp.get('temp_min'),
+                    'temp_max': temp.get('temp_max'),
+                    'humidity': weather.humidity,
+                    'wind_speed': wind.get('speed', 0.0),
+                    'wind_direction': self.deg_to_compass(wind.get('deg', 0)),
+                    'status': weather.detailed_status
+                }
+                weather_list.append(entry)
         
+        avg_temp = numpy.average([w['temperature'] for w in weather_list])
+        avg_humidity = numpy.average([w['humidity'] for w in weather_list])
+        avg_wind = numpy.average([w['wind_speed'] for w in weather_list])
+        temp_min = min(w['temp_min'] for w in weather_list)
+        temp_max = max(w['temp_max'] for w in weather_list)
+        status_summary = " ".join(
+            [f"{w['status']} at {w['time']}," for w in weather_list]
+        ).rstrip(',') + '.'
 
+        wind_dir = Counter([w['wind_direction'] for w in weather_list]).most_common(1)[0][0]
 
+        message = (
+            "Here's the weather for tomorrow: " \
+            f"Temperatures will range from {round(temp_min)} to {round(temp_max)} degrees, "
+            f"averaging around {round(avg_temp)} degrees. "
+            f"wind averaging {round(avg_wind, 1)} meters per a second from the {wind_dir.lower()}. "
+            f"Sunrise at {sunrise}, sunset at {sunset}."
+            f"There will be: {status_summary}"
+        )
 
-#Demo
-myWeather = Weather()
-weather = myWeather.weather
-myWeather.forecast()
+        return message
